d6ceebabf50bce657b935b9a0a2f2579cb15e531
Ryhan Brunello <ryhan.brunello@luizalabs.com>
2020-01-02 16:27:44 -0300

    Documentação

--- src/cliente/cliente.entity.ts
+++ src/cliente/cliente.entity.ts
@@ -1,26 +1,48 @@
 import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Pedido } from 'src/pedido/pedido.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Cliente {
+    @ApiProperty({
+        description: 'ID da cliente'
+    })
     @PrimaryGeneratedColumn()
     id: number;
 
+    @ApiProperty({
+        description: 'Nome'
+    })
     @Column({ length: 100 })
     nome: string;
 
+    @ApiProperty({
+        description: 'CPF/CNPJ'
+    })
     @Column({ length: 14, unique: true })
     cpfcnpj: string;
 
+    @ApiProperty({
+        description: 'E-Mail'
+    })
     @Column({ length: 100, unique: true })
     email: string;
 
+    @ApiProperty({
+        description: 'Senha'
+    })
     @Column({ length: 20 })
     senha: string;
 
+    @ApiProperty({
+        description: 'Ativo'
+    })
     @Column({ default: true })
     isActive: boolean;
     
+    @ApiProperty({
+        description: 'Pedidos'
+    })
     @OneToMany(type => Pedido, pedido => pedido.cliente)
     pedidos: Pedido[];
 }
